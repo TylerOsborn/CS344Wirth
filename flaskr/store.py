e728ce3ddee5426ecb9eddf7d57d5f37d0e10169
Tyler James osborn <21581118@sun.ac.za>
2021-08-31 16:21:02 +0200

    added imports and enabled debug

--- flaskr/store.py
+++ flaskr/store.py
@@ -1,8 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
     return render_template("store/home_page.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
