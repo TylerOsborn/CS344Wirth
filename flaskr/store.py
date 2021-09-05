2372d7ae4b9902c5c3c84aa00b597a983613b212
Tyler James osborn <21581118@sun.ac.za>
2021-09-05 14:22:04 +0200

    added route to item page

--- flaskr/store.py
+++ flaskr/store.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
+import pandas as pd
+import recommendation as recommendation
+
 app = Flask(__name__)
 
 
@@ -8,7 +11,11 @@ def home_page():
 
 @app.route("/product_search")
 def products_page():
-    return render_template("store/products_page.html")
+    return render_template("store/products_page.html", data=recommendation.products)
+
+@app.route("/item")
+def item_page():
+    return render_template("store/item_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
