586384c641b80b101505caeebd4ed4bf19d0e45c
Tyler James osborn <21581118@sun.ac.za>
2021-09-20 15:40:28 +0200

    added item page

--- flaskr/store.py
+++ flaskr/store.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 import pandas as pd
-import test_recommender as recommendation
+import test_recommender as recommender
 
 app = Flask(__name__)
 
@@ -13,15 +13,26 @@ def home_page():
 def products_page():
     return render_template("store/products_page.html", data=None, data_len=0)
 
-@app.route("/item")
+@app.route("/item", methods=["GET","POST"])
 def item_page():
-    return render_template("store/item_page.html")
+    if request.method == "GET":
+        product_id = request.args["product_id"]
+        recommendations = recommender.get_recommendations(product_id)
+        product = recommender.get_item(product_id)
+        return  render_template("store/item_page.html", product=product, recommendations=recommendations) 
+    return render_template("store/item_page.html", product=None, recommendations=None)
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
-    query = request.args['query']
-    print(query)
-    data = recommendation.search_products(query)
+    query = request.args["query"]
+    data = recommender.search_products(query)
     return render_template("store/products_page.html", data=data, data_len=len(data))
+
+@app.route("/product", methods=["GET", "POST"])
+def get_item_page():
+    query = request.args["query"]
+    recommendations = recommender.get_recommendations(query)
+    return "<h1> hello </h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
