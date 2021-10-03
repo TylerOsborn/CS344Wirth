92404974d6e64bb2a8173268b5672723dcf293c0
Lauren Abrahall <49614695+laurenabrahall@users.noreply.github.com>
2021-10-03 17:17:28 +0200

    Add cluster metric

--- flaskr/store.py
+++ flaskr/store.py
@@ -6,22 +6,29 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_page():
     return render_template("store/home_page.html")
+
 
 @app.route("/product_search")
 def products_page():
     return render_template("store/products_page.html", data=None, data_len=0)
 
-@app.route("/item", methods=["GET","POST"])
+
+@app.route("/item", methods=["GET", "POST"])
 def item_page():
     if request.method == "GET":
         product_id = request.args["product_id"]
         recommendations, scores = recommender.get_recommendations(product_id)
         product = recommender.get_item(product_id)
-        return  render_template("store/item_page.html", product=product, recommendations=recommendations, scores=scores) 
-    return render_template("store/item_page.html", product=None, recommendations=None)
+        clusters = [recommender.get_cluster(product_id)]
+        for i in range(len(recommendations)):
+            clusters.append(recommender.get_cluster(recommendations['catalogItemId'].iloc[i]))
+        return render_template("store/item_page.html", product=product, recommendations=recommendations, scores=scores, clusters=clusters)
+    return render_template("store/item_page.html", product=None, recommendations=None, clusters=None)
+
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
@@ -29,11 +36,13 @@ def search():
     data = recommender.search_products(query)
     return render_template("store/products_page.html", data=data, data_len=len(data))
 
+
 @app.route("/product", methods=["GET", "POST"])
 def get_item_page():
     query = request.args["query"]
     recommendations = recommender.get_recommendations(query, recommender.cosine_sim)
     return "<h1> hello </h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
